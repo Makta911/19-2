6dac5ead2a4e16e8586eb241d8138d0787f4382a
matvey <matvey.bosix@yandex.ru>
2024-08-06 00:11:39 +0800

    first commit

---
+++ catalog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        with open('temlplates/catalog/text.txt', 'a', encoding='utf-8') as file:
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            file.write(f"Имя пользователя : {name}\nТелефон: {phone}\nСообщение: {message}\n")
+            print(f"Имя пользователя : {name}\nТелефон: {phone}\nСообщение: {message}\n")
+
+    return render(request, 'catalog/contact.html')
