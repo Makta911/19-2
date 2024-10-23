3fd946a685a6fcf90c7537ddaa72c27dddccc37b
matvey <matvey.bosix@yandex.ru>
2024-10-23 22:17:36 +0800

    seven commit

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/base.html')
+    model = Product.objects.all()
+    context = {"product": model}
+    return render(request, 'catalog/product_list.html', context)
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product_detail.html', context)
+
 
 
 def contacts(request):
